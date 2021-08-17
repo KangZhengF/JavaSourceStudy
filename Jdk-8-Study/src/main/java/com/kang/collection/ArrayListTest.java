8911f8c8887982710cee6b4f5fe2beef7874c28c
康正锋 <xzhm00@163.com>
2021-08-17 11:07:32 +0800

    Associate local code with the remote repository

---
+++ Jdk-8-Study/src/main/java/com/kang/collection/ArrayListTest.java
@@ -0,0 +1,17 @@
+package com.kang.collection;
+
+import java.util.ArrayList;
+
+/**
+ * ArrayListTest class
+ *
+ * @author 康正锋
+ * @date 2021/08/17
+ */
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println(list);
+    }
+}
