04c948aa3b663a29d2664919324778062cd9a7b4
康正锋 <xzhm00@163.com>
2021-09-01 14:19:08 +0800

    给jdk源码加了注释没办法同步

--- Jdk-8-Study/src/main/java/com/kang/collection/ArrayListTest.java
+++ Jdk-8-Study/src/main/java/com/kang/collection/ArrayListTest.java
@@ -1,6 +1,8 @@
 package com.kang.collection;
 
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * ArrayListTest class
@@ -13,5 +15,22 @@ public class ArrayListTest {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         System.out.println(list);
+
+        //启动类加载器
+        System.out.println("启动类加载器");
+        String bootClassPath = System.getProperty("sun.boot.class.path");
+        System.out.println(bootClassPath);
+        //扩展类加载器
+        System.out.println("扩展类加载器");
+        String extPath = System.getProperty("java.ext.dirs");
+        System.out.println(extPath);
+        URLClassLoader extLoader = (URLClassLoader) ClassLoader.getSystemClassLoader().getParent();
+        System.out.println(Arrays.toString(extLoader.getURLs()));
+        //系统类加载器
+        System.out.println("系统类加载器");
+        String appPath = System.getProperty("java.class.path");
+        System.out.println(appPath);
+        URLClassLoader systemClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+        System.out.println(Arrays.toString(systemClassLoader.getURLs()));
     }
 }
