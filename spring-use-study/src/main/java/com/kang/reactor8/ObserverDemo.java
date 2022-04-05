2f6230a56e023c79229a5468ed6432142c8fabe6
康正锋 <2418438925@qq.com>
2022-04-05 15:24:14 +0800

    响应式编程1

---
+++ spring-use-study/src/main/java/com/kang/reactor8/ObserverDemo.java
@@ -0,0 +1,17 @@
+package com.kang.reactor8;
+
+import java.util.Observable;
+
+public class ObserverDemo extends Observable {
+    public static void main(String[] args) {
+        // Java8提供观察者模式的两个类Observer和Observable
+        ObserverDemo observer = new ObserverDemo();
+        // 添加观察者
+        observer.addObserver((o, arg) -> System.out.println("发生变化"));
+        observer.addObserver((o, arg) -> System.out.println("手动通知被观察者，准备改变"));
+        // 数据发生变化
+        observer.setChanged();
+        // 通知
+        observer.notifyObservers();
+    }
+}
