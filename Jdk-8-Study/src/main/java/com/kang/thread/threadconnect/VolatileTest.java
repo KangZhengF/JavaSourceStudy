f9078484cba805697ebe95fa389ee2bbebc84590
康正锋 <2418438925@qq.com>
2022-04-07 23:54:25 +0800

    读屏障

--- Jdk-8-Study/src/main/java/com/kang/thread/threadconnect/VolatileTest.java
+++ Jdk-8-Study/src/main/java/com/kang/thread/threadconnect/VolatileTest.java
@@ -1,11 +1,27 @@
 package com.kang.thread.threadconnect;
 
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
 /**
  * 美团面试题
  */
 public class VolatileTest {
     private static int x = 0, y = 0;
     private static int a = 0, b = 0;
+
+    public static Unsafe getUnsafe() {
+        Field field = null;
+        try {
+            field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            return (Unsafe) field.get(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public static void main(String[] args) throws InterruptedException {
         int i = 0;
@@ -17,10 +33,20 @@ public class VolatileTest {
             b = 0;
             Thread t1 = new Thread(() -> {
                 a = 1;
+                //写屏障
+                try {
+                    //真正起作用的是反射底层用的cas技术，cas会在指令前加lock前缀。
+                    Field field = VolatileTest.class.getDeclaredField("a");
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                }
+                VolatileTest.getUnsafe().storeFence();
                 x = b;
             });
             Thread t2 = new Thread(() -> {
                 b = 1;
+                // 写屏障
+                VolatileTest.getUnsafe().storeFence();
                 y = a;
             });
             t1.start();
