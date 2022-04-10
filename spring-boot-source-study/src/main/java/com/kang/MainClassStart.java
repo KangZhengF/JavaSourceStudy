ec1376079cc899c6dda3a633103b54267567b375
康正锋 <2418438925@qq.com>
2022-04-10 23:53:09 +0800

    springboot自动装配原理阅读

---
+++ spring-boot-source-study/src/main/java/com/kang/MainClassStart.java
@@ -0,0 +1,11 @@
+package com.kang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClassStart {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClassStart.class, args);
+    }
+}
---
+++ spring-boot-source-study/src/main/java/com/kang/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.kang.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String testHello() {
+        return "hello";
+    }
+}
